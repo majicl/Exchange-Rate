d96977bf8fd2f693b185ffd4ac1f96d0a5ea5481
Majid Parvin <majid.parvin@comeon.com>
2019-12-11 22:04:14 +0100

    initiate currency-controller with routes

---
+++ Src/ExchangeRate.API/Controllers/CurrencyController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExchangeRate.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CurrencyController : ControllerBase
+    {
+        [HttpGet("ExchangeRate")]
+        public IEnumerable<string> ExchangeRate()
+        {
+            return new string[] { "A" };
+        }
+    }
+}
